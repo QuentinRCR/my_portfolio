8ac6f92f50080e985f911ed33cc82a265dd0bb4d
Quentin REY <quentin.rey@etu.emse.fr>
2023-02-03 23:11:53 +0100

    add info of a project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Project from './components/Project'
 function App() {
   return (
     <div className="App">
-      <Project></Project>
+      <Project title={"Personal project - Consultation of bus schedules"}
+               backendURL={"https://github.com/QuentinRCR/backend-shuttle-time-display"}
+               frontendURL={"https://github.com/QuentinRCR/shuttle-time-display"}
+               dates={["January 2023","Today"]}
+               technologies={["React","Spring Boot"]}
+               videoURL={"https://drive.google.com/file/d/1H4_u8ao7DGZO2L2rPvkl3VUS00g7HXLJ/preview"}
+      ></Project>
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,16 @@
 import './Project.scss';
 import React from "react";
 
-function Project() {
+function Project({title,backendURL,frontendURL,dates,technologies,videoURL}) {
     return (
         <div className="project">
+            <h1>{title}</h1>
+            <h2>{dates}</h2>
+            <h2>{technologies}</h2>
+            <a href={backendURL}>Back-end repository</a>
+            <a href={frontendURL}>Front-end repository</a>
+            <iframe className="videoDemonstration" title="presentation appointment scheduler" src={videoURL} allowFullScreen={true}></iframe>
+
 
         </div>
 
