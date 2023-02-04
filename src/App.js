335947f77cfcb6f1f51cb5cec7d4437e04a82807
Quentin REY <quentin.rey@etu.emse.fr>
2023-02-04 23:35:11 +0100

    handle the display of several technologies and repository types

--- src/App.js
+++ src/App.js
@@ -40,16 +40,13 @@ function App() {
         {
             title: "Web interface to display the data of the ecological situation of Saint-Etienne",
             repositoriesURL: {
-                backendURL: null,
-                frontendURL: null
             },
             dates: {
                 start: "September 2021",
                 end: "June 2022"
             },
             technologies: {
-                frontend:"JavaScript",
-                backend:null
+                frontend:"JavaScript"
             },
             videoURL: "https://drive.google.com/file/d/1H4_u8ao7DGZO2L2rPvkl3VUS00g7HXLJ/preview",
             website: "https://donut.ctc-42.org/"
@@ -61,10 +58,9 @@ function App() {
             <Project
                     key={project.title}
                     title={project.title}
-                     backendURL={project.repositoriesURL.backend}
-                     frontendURL={project.repositoriesURL.frontend}
-                     dates={[project.dates.start,project.dates.end]}
-                     technologies={[project.technologies.frontend,project.technologies.backend]}
+                    repositories={project.repositoriesURL}
+                     dates={project.dates}
+                     technologies={project.technologies}
                      videoURL={project.videoURL}
             ></Project>
         )}
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,32 @@
 import './Project.scss';
 import React from "react";
 
-function Project({title,backendURL,frontendURL,dates,technologies,videoURL}) {
+function Project({title,repositories,dates,technologies,videoURL}) {
     return (
         <div className="project">
-            <h1>{title}</h1>
-            <h2>{dates}</h2>
-            {technologies ? <h2>{technologies}</h2> : null /*display the element if technologies in not null*/}
-            {backendURL ? <a href={backendURL}>Back-end repository</a> : null }
-            {frontendURL ? <a href={frontendURL}>Front-end repository</a> : null }
-            {videoURL ? <iframe className="videoDemonstration" title="presentation appointment scheduler" src={videoURL} allowFullScreen={true}></iframe> : null}
-
+            <div className="box">
+                <h1>{title}</h1>
+                <h2>{dates.start} - {dates.end}</h2>
+                {technologies ? /*display the element if technologies in not null*/
+                    <h3>Technologie used:
+                        <ul>
+                        {Object.keys(technologies)?.map((key,index) => /*For each item, display the name
+                         of the key with uppercase on the first character and the name of the techno*/
+                            <li key={index}>{key.charAt(0).toUpperCase() + key.slice(1)}: {technologies[key]}</li>)}
+                        </ul>
+                    </h3>
+                : null }
+                {repositories ? /*display the element if technologies in not null*/
+                    <h3>Repositories:
+                        <ul>
+                            {Object.keys(repositories)?.map((key) => /*For each item, display the name
+                         of the key with uppercase on the first character and repositories and add the correct link*/
+                                <li key={repositories[key]}><a href={repositories[key]}>{key.charAt(0).toUpperCase() + key.slice(1)} repository</a></li>)}
+                        </ul>
+                    </h3>
+                    : null }
+                {videoURL ? <iframe className="videoDemonstration" title="presentation appointment scheduler" src={videoURL} allowFullScreen={true}></iframe> : null}
+            </div>
 
         </div>
 
