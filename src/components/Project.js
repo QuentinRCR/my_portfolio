54011a266478886c92d788f66c576ed36e976a25
Quentin REY <quentin.rey@etu.emse.fr>
2023-02-05 00:00:16 +0100

    prettify cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,27 +4,29 @@ import React from "react";
 function Project({title,repositories,dates,technologies,videoURL}) {
     return (
     <div className="project">
-            <h1>{title}</h1>
-            <h2>{dates.start} - {dates.end}</h2>
+            <h2>{title}</h2>
+            <h3>{dates.start} - {dates.end}</h3>
             {technologies ? /*display the element if technologies in not null*/
-                <h3>Technologie used:
+                <h4>Technologie used:
                     <ul>
                     {Object.keys(technologies)?.map((key,index) => /*For each item, display the name
                      of the key with uppercase on the first character and the name of the techno*/
                         <li key={index}>{key.charAt(0).toUpperCase() + key.slice(1)}: {technologies[key]}</li>)}
                     </ul>
-                </h3>
+                </h4>
             : null }
             {Object.keys(repositories).length>0 ? /*display the element if technologies in not null*/
-                <h3>Repositories:
+                <h4 className="repositories">Repositories:
                     <ul>
                         {Object.keys(repositories)?.map((key) => /*For each item, display the name
                      of the key with uppercase on the first character and repositories and add the correct link*/
                             <li key={repositories[key]}><a href={repositories[key]}>{key.charAt(0).toUpperCase() + key.slice(1)} repository</a></li>)}
                     </ul>
-                </h3>
+                </h4>
                 : null }
-            {videoURL ? <iframe className="videoDemonstration" title="presentation appointment scheduler" src={videoURL} allowFullScreen={true}></iframe> : null}
+            {videoURL ?
+                <iframe className="videoDemonstration" title="presentation appointment scheduler" src={videoURL} allowFullScreen={true}></iframe>
+                : null}
 
 
         </div>
